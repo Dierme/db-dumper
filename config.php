3e540fcf7a7cd2e13a4b98a87a188a678e56e335
vagrant <vagrant@homestead>
2019-05-31 07:22:11 +0000

    dumping and logging errors

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'database' => [
+        'driver' => 'pgsql',
+        'database' => 'dbk',
+        'username' => 'homestead',
+        'password' => 'secret',
+        'dump_folder' => '/home/vagrant/code/db-dumper/dumps',
+    ],
+
+    'log' => [
+        'path' => '/home/vagrant/code/db-dumper/log'
+    ]
+];
---
+++ run.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'vendor/autoload.php';
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Spatie\DbDumper\Databases\PostgreSql;
+
+# Getting config
+$config = require('config.php');
+$database_conf = $config['database'];
+$log_conf = $config['log'];
+
+# Check if log file exists. If not - create one
+//$week_number = date('W', time());
+$full_file_name = $log_conf['path'] . '/' . 'week-' . date('W', time()) . '.log';
+if (!file_exists($full_file_name)) {
+    fopen($full_file_name, 'w');
+}
+
+# Initializing the logger
+$Logger = new Logger('db-dumper');
+$Logger->pushHandler(new StreamHandler($full_file_name, Logger::INFO));
+
+# Dumping the DB
+$Logger->info('Begin dump');
+$Dumper = PostgreSql::create();
+$dump_name = $database_conf['database'] . '-' . strval(time()) . '.sql';
+
+try {
+    $Dumper->setDbName($database_conf['database'])
+        ->setUserName($database_conf['username'])
+        ->setPassword($database_conf['password'])
+        ->dumpToFile($database_conf['dump_folder'] . '/' . $dump_name);
+} catch (\Exception $e) {
+    $Logger->error('Exception', array('exception' => $e));
+    throw $e;
+}
